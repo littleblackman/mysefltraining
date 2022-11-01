d5463fd515ed15147dadc4a4dc77922bbcb9cbf7
Sandy <sandyrazafitrimo@gmail.com>
2022-11-01 10:52:42 +0100

    update css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,20 @@
 
    <img src="{{URL::asset('storage/image/good-faces-640x426.jpg')}}" />
 
+    <h2 class="center">
+        MYST<br/>la plateforme qui vous aide à progresser
+    </h2>
+    <p>
+        Prenez le temps de revenir à la base :
+        <ul>
+            <li>Choisissez un cours</li>
+           <li>Démarrer la leçon</li>
+           <li>Vérifier votre compréhension</li>
+        </ul>
+
+        Et à la fin de chaque cours vous aurez une évalution de vos connaissances.
+    </p>
+
+    <button></button>
+
 @endsection
